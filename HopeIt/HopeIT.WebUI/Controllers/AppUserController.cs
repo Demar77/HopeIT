8b2b28021be2e827ae7e5c59c9411d6e919fc9b7
Łukasz Piszczałka <lpiszczalka@gmail.com>
2017-10-28 12:59:14 +0200

    WebUI for donators review

---
+++ HopeIt/HopeIT.WebUI/Controllers/AppUserController.cs
@@ -0,0 +1,47 @@
+﻿using HopeIT.Database.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HopeIT.WebUI.Controllers
+{
+    public class AppUserController : Controller
+    {
+        //// GET: AppUser
+        public ActionResult Index()
+        {
+            var repo = new AppUserRepo();
+            var result = repo.GetAppUser();
+            
+            return View(result);
+        }
+
+        // 
+        // GET: /AppUser/ 
+
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> <i>AppUser</i> action...";
+        //}
+
+        // 
+        // GET: /AppUser/List/ 
+
+        public string List()
+        {
+            return "This is the AppUser list action method...";
+        }
+
+        // 
+        // GET: /AppUser/List/ 
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ HopeIt/HopeIT.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HopeIT.WebUI.MvcApplication" Language="C#" %>
